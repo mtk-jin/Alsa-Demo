df12da008f0189a669b31ed88ab50f827e1ee302
qingyang8513 <44761102+qingyang8513@users.noreply.github.com>
2019-02-20 22:19:34 +0800

    Add files via upload

---
+++ interface.h
@@ -0,0 +1,20 @@
+#ifndef ALSA_INTERFACE_H
+#define ALSA_INTERFACE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
+#include <alsa/asoundlib.h>
+#include <memory.h>
+
+
+typedef struct PCMParameter {
+    int nChannels;
+    int nBits;
+    unsigned int sampleRate;
+    unsigned int framePerSample;
+}PCMParameter;
+
+
+#endif // ALSA_INTERFACE_H
